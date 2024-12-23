2aa0fe753585145f2c6d859fc7e030535230c057
binhtt920 <binhtt920@gmail.com>
2024-12-23 13:49:15 +0700

    Add topic_19_ IFrame_Frame

--- selenium/webDriver/Topic_18_DragAndDrop.java
+++ selenium/webDriver/Topic_18_DragAndDrop.java
@@ -1,11 +1,11 @@
 package webDriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -19,7 +19,7 @@ public class Topic_18_DragAndDrop {
 
     @BeforeClass
     public void beforeClass(){
-        driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         actions = new Actions(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -61,11 +61,50 @@ public class Topic_18_DragAndDrop {
 
     }
 
+    @Test
+    public void TC_03_IFrame_Framre(){
+        driver.get("https://toidicodedao.com/");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.cssSelector("div.fb_iframe_widget iframe")));
 
+        Assert.assertTrue(driver.findElement(By.cssSelector("div.fb_iframe_widget iframe")).isDisplayed());
+
+        driver.switchTo().frame(driver.findElement(By.cssSelector("div.fb_iframe_widget iframe")));
+
+        String numberFollower = driver.findElement(By.xpath("//a[@title='Tôi đi code dạo']/parent::div/following-sibling::div")).getText();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//a[@title='Tôi đi code dạo']/parent::div/following-sibling::div")).getText(),numberFollower);
+        //driver.findElement(By.xpath("//a[@title='Tôi đi code dạo']/parent::div/following-sibling::div")).getText();
+    }
+
+    @Test
+    public void TC_04_Frame_FormSite() throws InterruptedException {
+        Select select;
+
+        driver.get("https://www.formsite.com/templates/education/campus-safety-survey/");
+
+        driver.findElement(By.id("imageTemplateContainer")).click();
+        Thread.sleep(3000);
+
+        driver.switchTo().frame(driver.findElement(By.id("frame-one85593366")));
+
+        new Select(driver.findElement(By.xpath("//label[text()='Year ']/following-sibling::select"))).selectByVisibleText("Junior");
+
+        new Select(driver.findElement(By.xpath("//label[text()='Residence ']/following-sibling::select"))).selectByVisibleText("Off Campus");
+
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();",driver.findElement(By.xpath("//label[text()='Male']/preceding-sibling::input")));
+
+        Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Male']/preceding-sibling::input")).isSelected());
+        Thread.sleep(5000);
+
+
+
+
+    }
 
     @AfterClass
     public void afterClass (){
-       // driver.quit();
+       driver.quit();
     }
 
 }
